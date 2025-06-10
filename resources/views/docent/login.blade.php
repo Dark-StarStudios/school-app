094f774be6eff1ad4a6bb16b109587c70ca1b27a
abodydody <a.alyouins17200@gmail.com>
2025-06-10 12:25:30 +0200

    frontend update

--- resources/views/docent/login.blade.php
+++ resources/views/docent/login.blade.php
@@ -1,27 +1,46 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Docent Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-    <h1>Login Docent</h1>
 
-    @if ($errors->any())
-        <div style="color:red;">
-            {{ $errors->first() }}
+<body class="bg-light">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card shadow">
+                    <div class="card-header text-center bg-success text-white">
+                        <h3>Login Docent</h3>
+                    </div>
+                    <div class="card-body">
+                        @if ($errors->any())
+                        <div class="alert alert-danger">
+                            {{ $errors->first() }}
+                        </div>
+                        @endif
+
+                        <form method="POST" action="{{ route('docent.login') }}">
+                            @csrf
+
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email:</label>
+                                <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required autofocus>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Wachtwoord:</label>
+                                <input type="password" class="form-control" id="password" name="password" required>
+                            </div>
+
+                            <button type="submit" class="btn btn-success w-100">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-    @endif
-
-    <form method="POST" action="{{ route('docent.login') }}">
-        @csrf
-
-        <label>Email:</label><br>
-        <input type="email" name="email" value="{{ old('email') }}"><br><br>
-
-        <label>Wachtwoord:</label><br>
-        <input type="password" name="password"><br><br>
-
-        <button type="submit">Login</button>
-    </form>
+    </div>
 </body>
+
 </html>
