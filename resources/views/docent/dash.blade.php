3cae20aa12ed7f8136c4d31dceba6d6573abdda9
Dark-StarStudios <146312613+Dark-StarStudios@users.noreply.github.com>
2025-06-16 09:12:41 +0200

    made kind_register button

--- resources/views/docent/dash.blade.php
+++ resources/views/docent/dash.blade.php
@@ -7,7 +7,9 @@
     <title>Dashboard Kinderen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
+<header class="bg-success text-white p-4">
+    <a href="/kind/register" class="btn btn-light">Kind registreren</a>
+</header>
 <body class="bg-light">
     <div class="container mt-5">
         <div class="row justify-content-center">
